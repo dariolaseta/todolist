64ae332862e31bfe10db319480bb2bf5440543f8
dariolaseta <dario.laseta@gmail.com>
2023-12-12 12:07:48 +0100

    added api call & create item

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  apiUrl: string = 'http://localhost:3000/posts';
+  apiUrl: string = 'http://localhost:3000/posts/';
+
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(
     private http: HttpClient,
     ) { }
 
   getData() {
-    
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  addItem(body: any) :void {
+    this.http.post(this.apiUrl, body, {headers: this.headers}).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
   }
 }
