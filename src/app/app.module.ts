d8634b3b348fb6f6d08d0c8d3a9465d0ea6cc8df
dariolaseta <dario.laseta@gmail.com>
2023-12-12 10:49:46 +0100

    imported mat card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { TrashcanComponent } from './view/trashcan/trashcan.component';
     BrowserAnimationsModule,
 
     MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
